94e387025af9fce78eec009450fc11665ebfdb55
Cheng Hu <williamcheng.hu@gmail.com>
2020-07-30 09:41:37 +0300

    heroku preparations

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 morgan.token('body', (req, res) => {
   const body = req.body;
   return JSON.stringify(body);
@@ -97,7 +99,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
