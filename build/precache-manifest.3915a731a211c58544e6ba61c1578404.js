3a2f546f9c457c8d9b22752a751d94c0ae0bfa52
Cheng Hu <williamcheng.hu@gmail.com>
2020-07-30 19:17:37 +0300

    uibuild

---
+++ build/precache-manifest.3915a731a211c58544e6ba61c1578404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b4650bb57d0bf406ca5a6549d30dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "728e444246f98209b2f0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c442e966b86de687689",
+    "url": "/static/js/2.805ab005.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.805ab005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728e444246f98209b2f0",
+    "url": "/static/js/main.4d9edf1d.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
